a44a1b6690e7f42fca5ee79bc25db0156c50bb0e
Vanh48420389 <sivanh740@gmail.com>
2025-12-28 23:03:49 +0700

    commit

---
+++ PTIT_CNTT3_IT202_Session02/Session02_IT202_Bai2.sql
@@ -0,0 +1,16 @@
+
+USE QuanLyTruongHoc;
+
+CREATE TABLE Student (
+    StudentID INT NOT NULL,             -- Mã SV
+    FullName VARCHAR(100) NOT NULL,     -- Tên SV
+    PRIMARY KEY (StudentID)             
+);
+
+CREATE TABLE Subject (
+    SubjectID INT NOT NULL,             -- Mã môn học
+    SubjectName VARCHAR(100) NOT NULL,  -- Tên môn học
+    Credits INT NOT NULL,               -- Số tín chỉ
+    PRIMARY KEY (SubjectID),
+	CHECK (Credits > 0)                 
+);
